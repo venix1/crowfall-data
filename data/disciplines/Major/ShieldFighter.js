4befb4b4e3f62fe0dc8639c813da649df5b54e04
Orleans <og.orleans@gmail.com>
2017-05-26 07:25:43 +0000

    Add Secutor and ShieldFighter major disciplines.

---
+++ data/disciplines/Major/ShieldFighter.js
@@ -0,0 +1,16 @@
+
+exports.discipline = {
+  name: "Shield Fighter",
+  type: "Major",
+  description: "Strength is not the only deciding factor in the heat of battle. Endurance, stamina, and tenacious spirit are necessary aspects of combat survival, and the addition of a shield can help increase the heartiness of the fighter. Shield Fighters are quick to utilize their armament by blocking blows, richocheting strikes, and the avoidance of massive damage. Fortitude, patience, and a defensive stance are what define a true Shield Fighter.",
+  icon: "",
+  can_equip: ["Cleric","Druid","Knight"],
+  stats_granted: ["Armor Penetration: Crushing"],
+  stats_values: [6.25],
+  equips_granted: ["Small Bucklers"],
+  slots_granted: ["Off-hand Weapon Slot"],
+  slots_removed: [],
+  trays_granted: "",
+  trays_removed: "",
+  powers_granted: ["Brutal Slam","Throw Illusion Shield","Mighty Shield Slam","Molon Labe"]
+}
