c3458c7e2ac80f9d2e6d4ea63aaa40b549709c51
Orleans <og.orleans@gmail.com>
2017-05-26 05:32:54 +0000

    BladeMaster, Destroyer and Dryad major disciplines added. Template added.

---
+++ data/disciplines/Major/Destroyer.js
@@ -0,0 +1,16 @@
+
+exports.discipline = {
+  name: "Destroyer",
+  type: "Major",
+  description: "The beauty of chaos and destruction can only be seen by the few who allow themselves to succumb to their rage. Through the channeling of rage and devastation the ability to maintain a relentless and frightening vitality occurs. Destroyers embrace their destructive ways and are often granted exceptional and frightening abilities in combat.",
+  icon: "",
+  can_equip: ["Champion"],
+  stats_granted: ["Max Rage"],
+  stats_values: [8.75],
+  equips_granted: [],
+  slots_granted: [],
+  slots_removed: [],
+  trays_granted: "",
+  trays_removed: "",
+  powers_granted: ["Aegis of Salvation","Insanity","Frighten","Relentless"]
+}
