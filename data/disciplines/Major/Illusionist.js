1a4b7c908aa68458e3d7419847f010b4943ac3a1
Orleans <og.orleans@gmail.com>
2017-05-26 06:02:41 +0000

    Add FireWalker, Holy Avenger and Illusionist.

---
+++ data/disciplines/Major/Illusionist.js
@@ -0,0 +1,16 @@
+
+exports.discipline = {
+  name: "Illusionist",
+  type: "Major",
+  description: "An illusionist alters the perception of those within their proximity which allows for easier deception and confusion. With the assitance of a magic weapon and the finest of accoutrements an illusionist maintains a dapper appearance while also being a worthy adversary in combat.",
+  icon: "",
+  can_equip: ["Assassin","Champion","Cleric","Confessor","Druid","Duelist","Frostweaver","Knight","Myrmidon","Ranger","Templar"],
+  stats_granted: ["Perception"],
+  stats_values: [20],
+  equips_granted: [],
+  slots_granted: [],
+  slots_removed: [],
+  trays_granted: "",
+  trays_removed: "",
+  powers_granted: ["Glamour Weapon","Reveal Shadows","Finery"]
+}
