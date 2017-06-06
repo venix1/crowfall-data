790bceefb6633b780b908b13bf515163a37fc140
Michael C <MichaelFedora@users.noreply.github.com>
2017-06-06 12:37:37 -0400

    Friar description issue
    remove trailing `: `

--- data/disciplines/Major/Friar.js
+++ data/disciplines/Major/Friar.js
@@ -1,7 +1,7 @@
 exports.discipline = {
   name: "Friar",
   type: "Major",
-  description: "Friars of the Church of Arkon provide the community with a unique service that no other can provide.  Friars willingly listen and pardon the sins of the people with an undaunting zeal; no sin is too great, for they have heard them all and they are eager to hear more.  Their unyielding dedication to their trade provides many in their company with benefits in health and the avoidance of sin while also providing the Friars with a few extra perks.: ",
+  description: "Friars of the Church of Arkon provide the community with a unique service that no other can provide.  Friars willingly listen and pardon the sins of the people with an undaunting zeal; no sin is too great, for they have heard them all and they are eager to hear more.  Their unyielding dedication to their trade provides many in their company with benefits in health and the avoidance of sin while also providing the Friars with a few extra perks.",
   can_equip: ["Assassin","Champion","Cleric","Confessor","Druid","Duelist","Frostweaver","Knight","Myrmidon","Ranger","Templar"],
   stats_granted: ["Final Healing Modifier"],
   stats_values: [0.063],
