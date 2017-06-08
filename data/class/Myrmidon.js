8e174f363539799a03f680cd9db35112c8953e2b
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2017-06-07 22:28:49 -0400

    adding powers granted list

--- data/class/Myrmidon.js
+++ data/class/Myrmidon.js
@@ -3,5 +3,6 @@ exports.class = {
   name: "Myrmidon",
   description: "",
   icon: "https://crowfall.wiki/images/class/myrmidon.png",
+  powers_granted: ["Arcing Slash", "Berserk", "Bloodied Swipe", "Bull Rush", "Cancel Combo", "Cast Net", "Charge", "Colossus Smash", "Dodge", "Double Smash", "Gore", "Left Slash", "Net Pull", "Pulverize", "Raging Bull", "Retaliate (Myrmidon)", "Right Slash", "Taste for Blood", "Whirlwind"],
   races: ["Stoneborn","Half-Giant","Minotaur"]
 };
