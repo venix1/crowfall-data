b06667973d65cd37ef38520beba5010ee253a018
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2017-06-08 22:19:52 -0400

    fixing missing semi

--- data/class/Templar.js
+++ data/class/Templar.js
@@ -1,8 +1,8 @@
 
 exports.class = {
-  name: "Templar",
-  description: "",
-  icon: "https://crowfall.wiki/images/class/templar.png",
-  powers_granted: ['Brilliance', 'Castigate', 'Censure', 'Devotion', 'Divine Light', 'Execute', 'Faith', 'Great Swing 1', 'Great Swing 2', 'Great Swing 3', 'Holy Warrior', 'Judgement', 'Radiant Sweep', 'Reproach', 'Retaliate (Templar)', 'Righteous Smash', 'Righteous Stand']
-  races: ["Human","Nethari","Elken"]
+  name: 'Templar',
+  description: '',
+  icon: 'https://crowfall.wiki/images/class/templar.png',
+  powers_granted: ['Brilliance', 'Castigate', 'Censure', 'Devotion', 'Divine Light', 'Execute', 'Faith', 'Great Swing 1', 'Great Swing 2', 'Great Swing 3', 'Holy Warrior', 'Judgement', 'Radiant Sweep', 'Reproach', 'Retaliate (Templar)', 'Righteous Smash', 'Righteous Stand'],
+  races: ['Human', 'Nethari', 'Elken']
 };
