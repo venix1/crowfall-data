6bce0bc0872b9d174a1db3c7adf8802cc5a8587d
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2017-06-13 22:30:35 -0400

    more corrections

--- data/class/Druid.js
+++ data/class/Druid.js
@@ -3,6 +3,6 @@ exports.class = {
   name: "Druid",
   description: "",
   icon: "https://crowfall.wiki/images/class/druid.png",
-  powers_granted: ["Coalesce Life", "Dissipate", "Leeching Seed", "Nature's Avatar", "Will O' Wisps", "Healing Rain", "Bark Skin", "Nature's Grace", "Essence Scram", "Forest Whispers"],
+  powers_granted: ["Aurora Emitter", "Bark Skin", "Coalesce Life", "Coalesce Life 2", "Coalesce Nature", "Contain Essence", "Dissipate", "Empower", "Essence Scram", "Faefire Fire", "Focused Spark", "Forest Whispers", "Gaea's Wail", "Healing Rain", "Leeching Seed", "Lightning Burst", "Lightning Strike", "Natural Bond", "Nature's Avatar", "Nature's Grace", "Sacrifice", "Shroud of Darkness", "Wicked Winds", "Will o' Wisps"],
   races: ["Half-Elf","Fae","Wood-Elf"]
 };
--- data/class/Ranger.js
+++ data/class/Ranger.js
@@ -3,5 +3,6 @@ exports.class = {
   name: "Ranger",
   description: "",
   icon: "https://crowfall.wiki/images/class/ranger.png",
+  powers_granted: ["Archers Stake", "Barrage", "Cross Slash", "Cross Slash 2", "Dagger Spin", "Dagger Spin 2", "Dagger Spin 3", "Dagger Spin 4", "Disengage", "Explosive  Trap", "Forest Step", "Gleeful Strike", "Jab", "Laceration", "Launch Explosive Trap", "Multi-Purpose Shot", "Power Shot", "Rapid Fire", "Retaliate (Ranger)", "Ricochet Shot", "Sinister Slash", "Spin Step", "Straight Shot", "Suppression Shot", "Sustain", "Sweep", "Tendon Slice", "Twin Surge", "Vile Slash", "Wicked Slash"],
   races: ["Human","Half-Elf","Wood-Elf","Elken","Minotaur"]
 };
