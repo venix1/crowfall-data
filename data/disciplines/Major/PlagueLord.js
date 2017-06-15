f2e8b0c199c357606299536633d06fe646b1a008
Michael C <MichaelFedora@users.noreply.github.com>
2017-06-15 15:06:29 -0400

    Fixed Plague Lord
    Removed excess `""` in stats_granted

--- data/disciplines/Major/PlagueLord.js
+++ data/disciplines/Major/PlagueLord.js
@@ -4,7 +4,7 @@ exports.discipline = {
   description: "As a host of the most noxious of contaminants your popularity may ebb and flow as you infect your enemies. The odors and muck you may leave behind will help remind those of your legacy as a plague bearer and a master of the rank andf oul making you one highly feared adversary.",
   icon: "",
   can_equip: ["Assassin","Champion","Cleric","Confessor","Druid","Duelist","Frostweaver","Knight","Myrmidon","Ranger","Templar"],
-  stats_granted: [""],
+  stats_granted: [],
   stats_values: [],
   equips_granted: [],
   slots_granted: [],
