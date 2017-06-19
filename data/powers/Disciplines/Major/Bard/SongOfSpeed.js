738a5a3dbd4a61f0e4f05a5844569c39bd483458
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2017-06-18 21:26:10 -0400

    convert hanging buff and temporary buff to just plain old buff

--- data/powers/Disciplines/Major/Bard/SongOfSpeed.js
+++ data/powers/Disciplines/Major/Bard/SongOfSpeed.js
@@ -1,13 +1,13 @@
 exports.power = {
-  name: "Song of Speed",
+  name: 'Song of Speed',
   sources: [
-    { type: "discipline", sub_type: "major", id: "bard" }
+    { type: 'discipline', sub_type: 'major', id: 'bard' }
   ],
-  type: "Temporary Buff",
-  cast_type: "Toggle",
+  type: 'Buff',
+  cast_type: 'Toggle',
   duration: 12,
   cooldown: 1,
-  targeting: "Group",
+  targeting: 'Group',
   max_targets: 5,
   range: 0,
   next_chain: [],
@@ -15,6 +15,6 @@ exports.power = {
     pips: 0,
     resource: 0
   },
-  tooltip: "While Out of Combat increases the groups Movement Speed by 35% for 12 seconds. While In Combat increases the groups Movement Speed by 10% for 12 seconds. Type 1 Song.",
-  icon: ""
+  tooltip: 'While Out of Combat increases the groups Movement Speed by 35% for 12 seconds. While In Combat increases the groups Movement Speed by 10% for 12 seconds. Type 1 Song.',
+  icon: ''
 };
--- data/powers/Disciplines/Major/Bard/VersesOfVictory.js
+++ data/powers/Disciplines/Major/Bard/VersesOfVictory.js
@@ -1,13 +1,13 @@
 exports.power = {
-  name: "Verses of Victory",
+  name: 'Verses of Victory',
   sources: [
-    { type: "discipline", sub_type: "major", id: "bard" }
+    { type: 'discipline', sub_type: 'major', id: 'bard' }
   ],
-  type: "Temporary Buff",
-  cast_type: "Toggle",
+  type: 'Buff',
+  cast_type: 'Toggle',
   duration: 12,
   cooldown: 1,
-  targeting: "Group",
+  targeting: 'Group',
   max_targets: 5,
   range: 0,
   next_chain: [],
@@ -15,6 +15,6 @@ exports.power = {
     pips: 0,
     resource: 0
   },
-  tooltip: "Increases your groups Attack Power by 150 as well as Support Power by 150 for 12 seconds. Type 4 Song.",
-  icon: ""
+  tooltip: 'Increases your groups Attack Power by 150 as well as Support Power by 150 for 12 seconds. Type 4 Song.',
+  icon: ''
 };
