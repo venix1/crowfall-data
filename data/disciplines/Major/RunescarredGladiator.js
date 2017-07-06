c0895a052659a832208eb45cdedf666391796171
MichaelFedora <mtc120@gmail.com>
2017-07-06 11:48:55 -0400

    Added cleric to useable classes in Runescarred Gladiator

--- data/disciplines/Major/RunescarredGladiator.js
+++ data/disciplines/Major/RunescarredGladiator.js
@@ -3,7 +3,7 @@ exports.discipline = {
   type: 'Major',
   description: 'Blood is used in rune making as a magical property of life essence and sacrifice. The ultimate sacrifice for rune crafting is scarring runes into the flesh, which only the most devote are brave enough to do. Exchanging small spaces in the flesh to fill with runes grants them unyielding power and a continued devotion to self-sacrificing for the sake of combat.',
   icon: 'https://cdn.malekai.network/images/disciplines/runescarred-gladiator.png',
-  classes: ['champion'],
+  classes: ['champion','cleric'],
   stats_granted: ['Max Rage', 'Enhanced Catch Your Breath'],
   stats_values: [10, 80],
   equips_granted: [],
